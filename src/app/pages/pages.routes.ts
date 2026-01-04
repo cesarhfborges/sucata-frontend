098b72a37c1a1f85bfa7c2e8980e41f8fda24df1
César Borges <cesar_silk321@hotmail.com>
2026-01-04 12:06:23 -0300

    primeiro

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Documentation } from './documentation/documentation';
 import { Crud } from './crud/crud';
 import { Empty } from './empty/empty';
+import { Home } from '@/pages/home/home';
 
 export default [
-    { path: 'documentation', component: Documentation },
-    { path: 'crud', component: Crud },
-    { path: 'empty', component: Empty },
-    { path: '**', redirectTo: '/notfound' }
+    // { path: 'documentation', component: Documentation },
+    // { path: 'crud', component: Crud },
+    // { path: 'empty', component: Empty },
+    { path: 'home', component: Home },
+    // { path: '**', redirectTo: '/notfound' }
 ] as Routes;
